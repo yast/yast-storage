ea56bfff8b43e803666ad6eb9f43be52f645c43a
Thomas Fehr <fehr@suse.de>
2003-07-31 09:58:03 +0000

    - merge fdisk, lvm and md agent into one storage agent - version 2.8.4
    svn path=/trunk/storage/; revision=10777

--- agent-fdisk/src/Y2CCStorageAgent.cc
+++ agent-fdisk/src/Y2CCStorageAgent.cc
@@ -10,9 +10,20 @@
 #include <scr/SCRInterpreter.h>
 
 #include "FdiskAgent.h"
+#include "LvmAgent.h"
+#include "MdAgent.h"
 
 
 typedef Y2AgentComp <FdiskAgent> Y2FdiskAgentComp;
 
 Y2CCAgentComp <Y2FdiskAgentComp> g_y2ccag_fdisk ("ag_fdisk");
+
+typedef Y2AgentComp <LvmAgent> Y2LvmAgentComp;
+
+Y2CCAgentComp <Y2LvmAgentComp> g_y2ccag_lvm ("ag_lvm");
+
+typedef Y2AgentComp <MdAgent> Y2MdAgentComp;
+
+Y2CCAgentComp <Y2MdAgentComp> g_y2ccag_md ("ag_md");
+
 
