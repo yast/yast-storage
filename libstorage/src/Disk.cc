a77d2752aea14ce2526af0ecfc94dcdb479e6d8d
Arvin Schnell <aschnell@suse.de>
2009-05-12 13:01:34 +0000

    - support EFI boot on non-ia64 systems (fate #306032)
    svn path=/branches/SuSE-SLE-10-SP3-Branch/storage/; revision=57136

--- libstorage/src/Disk.cc
+++ libstorage/src/Disk.cc
@@ -413,7 +413,7 @@ bool Disk::detectPartitions( ProcPart& ppart )
     if( detected_label.empty() )
 	detected_label = dlabel;
     if( dlabel.empty() )
-	dlabel = defaultLabel(size_k);
+	dlabel = defaultLabel(getStorage(), size_k);
     setLabelData( dlabel );
 
     if (label == "unsupported")
@@ -965,16 +965,18 @@ bool Disk::haveBsdPart(const list<Partition*>& pl) const
 #define EB (1024ULL * 1024ULL * 1024ULL * 1024ULL)
 #define PB (1024ULL * 1024ULL * 1024ULL * 1024ULL * 1024ULL)
 
-string Disk::defaultLabel( unsigned long long size_k )
+string Disk::defaultLabel(const Storage& storage, unsigned long long size_k)
     {
     string ret = "msdos";
-    if( Storage::arch()=="ia64" )
+    if (storage.efiBoot())
+        ret = "gpt";
+    else if( Storage::arch()=="ia64" )
 	ret = "gpt";
     else if( Storage::arch()=="sparc" )
 	ret = "sun";
     if( size_k>2*TB )
 	ret = "gpt";
-    y2milestone( "size_k:%llu ret:%s", size_k, ret.c_str() );
+    y2mil("efiboot:" << storage.efiBoot() << " size_k:" << size_k << " ret:" << ret);
     return( ret );
     }
 
