5bd776deadea0eecc53789665fcd341b2402be52
Arvin Schnell <aschnell@suse.de>
2009-12-07 15:45:41 +0000

    - started work on testsuite for storage proposal
    svn path=/trunk/storage/; revision=59984

--- storage/src/testsuite/tests/runtest.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-(/usr/lib/YaST2/bin/y2base -l /dev/fd/2 $1 $2 testsuite >$3) 2>&1 | fgrep -v " <0> " | grep -v "^$" | sed 's/^....-..-.. ..:..:.. [^)]*) //g' > $4
--- storage/src/testsuite/tests_propose/runtest_part.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-#echo XXXXXXXXXXXXXXXXXXXXXXXXXX $1 $2 $3 
-(/usr/lib/YaST2/bin/y2base -l /dev/fd/1 $1 testsuite 2>&1 ) | grep "OUT:" | grep -v "^$" | sed 's/^....-..-.. ..:..:.. [^)]*).*OUT:/  /g' >"$2" 2>"$3"
