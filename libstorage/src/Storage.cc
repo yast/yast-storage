c93ddf4b8892cc2a2f55146a7b62007e5d922b24
Arvin Schnell <aschnell@suse.de>
2010-01-25 16:32:59 +0000

    - fixed default mount-by for multipath (bnc #572209)
    svn path=/branches/SuSE-Code-11-SP1-Branch/storage/; revision=60522

--- libstorage/src/Storage.cc
+++ libstorage/src/Storage.cc
@@ -2523,11 +2523,33 @@ Storage::getMountBy( const string& device, MountByType& mby )
 	mby = defaultMountBy;
 	pair<string,unsigned> dp = Disk::getDiskPartition(device);
 	y2mil( "dp:" << dp );
-	DiskIterator i = findDisk( dp.first );
-	if( (i==dEnd()) || (mby == MOUNTBY_ID && i->udevId().empty()) ||
-	    (mby == MOUNTBY_PATH && i->udevPath().empty()))
-	    mby = MOUNTBY_DEVICE;
-	ret = STORAGE_VOLUME_NOT_FOUND;
+
+	DiskIterator i1 = findDisk(dp.first);
+	DmPartCoIterator i2 = findDmPartCo(dp.first);
+	MdPartCoIterator i3 = findMdPartCo(dp.first);
+
+	if (i1 != dEnd())
+	{ 
+	    if ((mby == MOUNTBY_ID && i1->udevId().empty()) ||
+		(mby == MOUNTBY_PATH && i1->udevPath().empty()))
+		mby = MOUNTBY_DEVICE;
+	}
+	else if (i2 != dmpCoEnd())
+	{
+	    if ((mby == MOUNTBY_ID && i2->udevId().empty()) ||
+		(mby == MOUNTBY_PATH && i2->udevPath().empty()))
+		mby = MOUNTBY_DEVICE;
+	}
+	else if (i3 != mdpCoEnd())
+	{
+	    if ((mby == MOUNTBY_ID && i3->udevId().empty()) ||
+		(mby == MOUNTBY_PATH && i3->udevPath().empty()))
+		mby = MOUNTBY_DEVICE;
+	}
+	else
+	{
+	    ret = STORAGE_VOLUME_NOT_FOUND;
+	}
 	}
     y2milestone( "ret:%d mby:%s", ret, Volume::mbyTypeString(mby).c_str());
     return( ret );
