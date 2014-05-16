6332a8ed3a2fa2d1df01b9ea5d9a2b6963abd0a5
Arvin Schnell <aschnell@suse.de>
2014-05-16 17:47:48 +0200

    - added more tests for "flex-proposal"

---
+++ testsuite/tests/flex-empty-ppc64le1.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+# testedfiles: helper.rb
+
+module Yast
+
+  class TestClient < Client
+
+    def main
+
+      def setup1()
+        setup_system("empty-ppc64le")
+
+        setup_part_info(<<-EOT)
+PARTITION  mount=/      size=8GB
+PARTITION  mount=swap   size=1GB
+PARTITION  mount=/data  sizepct=100
+EOT
+      end
+
+      def setup2()
+      end
+
+      def setup3()
+      end
+
+      Yast.include self, "helper.rb"
+
+    end
+
+  end
+
+end
+
+Yast::TestClient.new.main
