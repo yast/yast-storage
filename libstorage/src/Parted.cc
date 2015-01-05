f642625b0e5418e2f292c8917c3e589f201ad1fa
Arvin Schnell <aschnell@suse.de>
2015-01-05 13:18:08 +0100

    - support PReP on GPT in expert partitioner (fate#317611)

--- libstorage/src/Parted.cc
+++ libstorage/src/Parted.cc
@@ -339,6 +339,10 @@ namespace storage
 	    {
 		entry.id = Partition::ID_APPLE_HFS;
 	    }
+	    if (contains(flags, "prep"))
+	    {
+		entry.id = Partition::ID_GPT_PREP;
+	    }
 	}
 	y2mil("num:" << entry.num << " id:" << entry.id << " type:" << toString(entry.type));
 
