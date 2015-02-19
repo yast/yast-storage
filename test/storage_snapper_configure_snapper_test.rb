3e81651056aa0601fb03edfc46a8a1d1b0e8d674
Arvin Schnell <aschnell@suse.de>
2015-02-19 12:30:18 +0100

    - added checks

--- test/storage_snapper_configure_snapper_test.rb
+++ test/storage_snapper_configure_snapper_test.rb
@@ -42,4 +42,34 @@ describe "StorageSnapper#ConfigureSnapper?" do
   end
 
 
+  it "does not configure snapper" do
+
+    data = {
+      "device" => "/dev/sda1",
+      "mount" => "/",
+      "used_fs" => :xfs,
+      "userdata" => { "/" => "snapshots" }
+    }
+
+    Yast::Storage.stub(:GetEntryForMountpoint).with("/").once.and_return(data)
+
+    expect(Yast::StorageSnapper.ConfigureSnapper?).to eq false
+
+  end
+
+
+  it "does not configure snapper" do
+
+    data = {
+      "device" => "/dev/sda1",
+      "mount" => "/",
+      "used_fs" => :xfs
+    }
+
+    Yast::Storage.stub(:GetEntryForMountpoint).with("/").once.and_return(data)
+
+    expect(Yast::StorageSnapper.ConfigureSnapper?).to eq false
+
+  end
+
 end
