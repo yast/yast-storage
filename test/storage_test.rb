85d0fd77aa2f99eda553061bc37b9fac0844efdb
Lukas Ocilka <lukas.ocilka@gmail.com>
2015-08-19 15:15:17 +0200

    Test case for new Storage.SetUserData function

---
+++ test/storage_test.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env rspec
+
+ENV["Y2DIR"] = File.expand_path("../../src", __FILE__)
+
+require "yast"
+
+Yast.import "Storage"
+Yast.import "StorageInit"
+
+describe "Yast::Storage" do
+  before do
+    Yast::Storage.InitLibstorage(false)
+  end
+
+  describe "#SetUserData" do
+    it "sets given user data for a given device" do
+      # non-zero error for device that does not exist
+      expect(Yast::Storage.SetUserData("/dev/ice/does/not/exist", { "/" => "snapshots" })).not_to eq(0)
+    end
+  end
+end
