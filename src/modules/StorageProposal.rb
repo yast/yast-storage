d08c940112025ebee6d68534124ab557c480d714
Dinar Valeev <dvaleev@suse.com>
2015-09-11 16:41:30 +0200

    Propose /boot for encypted root on PowerPC
    PowerPC should be handled specially similar what is done with Efi.
    We need to define boot2, so /boot will be proposed for encrypted root
    
    Signed-off-by: Dinar Valeev <dvaleev@suse.com>

--- src/modules/StorageProposal.rb
+++ src/modules/StorageProposal.rb
@@ -5624,11 +5624,16 @@ module Yast
       Ops.set(ret, "target", target)
 
       boot2 = {}
-      if GetProposalEncrypt() && Partitions.EfiBoot
+      if GetProposalEncrypt() && (Partitions.EfiBoot||Partitions.PrepBoot)
+        if Partitions.EfiBoot
+          bootfsid = Partitions.DefaultFs
+        else
+          bootfsid = Partitions.DefaultBootFs
+        end
         boot2 = {
           "mount" => "/boot",
           "size"  => 400 * 1024 * 1024,
-          "fsys"  => Partitions.DefaultFs,
+          "fsys"  => bootfsid,
           "id"    => Partitions.fsid_native
         }
       end
@@ -6009,11 +6014,16 @@ module Yast
       Ops.set(ret, "target", target)
 
       boot2 = {}
-      if GetProposalEncrypt() && Partitions.EfiBoot
+      if GetProposalEncrypt() && (Partitions.EfiBoot && Partitions.PrepBoot)
+        if Partitions.EfiBoot
+          bootfsid = Partitions.DefaultFs
+        else
+          bootfsid = Partitions.DefaultBootFs
+        end
         boot2 = {
           "mount" => "/boot",
           "size"  => Partitions.ProposedBootsize,
-          "fsys"  => Partitions.DefaultFs,
+          "fsys"  => bootfsid,
           "id"    => Partitions.fsid_native
         }
       end
