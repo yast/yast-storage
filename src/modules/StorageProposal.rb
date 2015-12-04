c374f5b7800753f39309c9c528d88f7d79a5c250
Jiri Srain <jsrain@suse.cz>
2015-12-04 09:33:49 +0100

    added a comment, version bump

--- src/modules/StorageProposal.rb
+++ src/modules/StorageProposal.rb
@@ -6116,6 +6116,8 @@ module Yast
 
 
     def get_inst_prop(target)
+      # initialize data from control file earlier, it is needed in this function
+      # to decide whteher to use LVM proposal (bsc#957913)
       GetControlCfg()
       target = deep_copy(target)
       ret = {}
